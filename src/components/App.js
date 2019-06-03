c9e92d7c4dcd2091004a22d59c33cd88a856c7cd
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-03 07:20:02 -0600

    Created src/components and moved App to it

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <p>Welcome to the Stardew Valley Quick Reference</p>
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
