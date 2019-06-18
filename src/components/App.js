3e9af00d2747e700253cc0e083430d174f9619bb
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-18 02:33:03 -0600

    Added style reset and basic landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import LoadingIcon from './LoadingIcon'
+import '../App.css'
 
-// All routing components to be lazy loaded should be like this
-// const Home = React.lazy(() => import('./Home'))
+// All routing components should be lazy loaded like this
+const Home = React.lazy(() => import('./Home'))
 // const Other = React.lazy(() => import('./Other'))
 
 function App() {
   return (
     <Router>
-      <LoadingIcon />
+      {/* <LoadingIcon /> */}
+      {/* All routes must go within suspense to support lazy loading */}
       <React.Suspense fallback={<LoadingIcon />}>
-        {/* All routes must go within suspense to support lazy loading */}
-        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/" component={Home} />
         {/* <Route path="/other" component={Other} /> */}
       </React.Suspense>
     </Router>
