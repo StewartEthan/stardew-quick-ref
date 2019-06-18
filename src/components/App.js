c470aaeb035133a2e0f4fa2004c2ccac91d2581d
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-18 00:23:11 -0600

    Added temporary loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import LoadingIcon from './LoadingIcon'
 
 // All routing components to be lazy loaded should be like this
 // const Home = React.lazy(() => import('./Home'))
@@ -8,8 +9,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 function App() {
   return (
     <Router>
-      {/* TODO: Suspense fallback loading component */}
-      <React.Suspense fallback="Loading...">
+      <LoadingIcon />
+      <React.Suspense fallback={<LoadingIcon />}>
         {/* All routes must go within suspense to support lazy loading */}
         {/* <Route exact path="/" component={Home} /> */}
         {/* <Route path="/other" component={Other} /> */}
---
+++ src/components/LoadingIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+const style = css`
+  
+`
+
+export default function LoadingIcon() {
+  return <div>Loading...</div>
+}
