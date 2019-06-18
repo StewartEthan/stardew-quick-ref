af17ebcc83ed57a90dadf3351b4dcfecea41621c
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-18 02:37:34 -0600

    HTML formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-      <p>Welcome to the Stardew Valley Quick Reference! This is intended to be an easy-to-navigate quick lookup tool for Stardew Valley. While I love the official wiki and highly encourage everyone to use it, I find it sometimes annoying to navigate on a mobile device. I usually use my phone to look things up in the wiki while I play, so I notice it frequently. If I was most recently on a villager's page, and then come back to look up when you can catch a largemouth bass, I have to either know the URL or use the search function. I've been itching to do a side project for a little while, so I decided to make an alternative reference, with pages more lightweight and focused on what I usually need to look up while playing, and a focus on easier navigation from any random page to any other random page (especially on mobile).</p>
+      <p>
+        Welcome to the Stardew Valley Quick Reference! This is intended to be an easy-to-navigate quick lookup tool for Stardew Valley. While I love <a href="https://stardewvalleywiki.com/Stardew_Valley_Wiki" target="_blank">the official wiki</a> and highly encourage everyone to use it, I find it sometimes annoying to navigate on a mobile device. I usually use my phone to look things up in the wiki while I play, so I notice it frequently. If I was most recently on a villager's page, and then come back to look up when you can catch a largemouth bass, I have to either know the URL or use the search function. I've been itching to do a side project for a little while, so I decided to make an alternative reference, with pages more lightweight and focused on what I usually need to look up while playing, and a focus on easier navigation from any random page to any other random page (especially on mobile).
+      </p>
     </>
   )
 }
