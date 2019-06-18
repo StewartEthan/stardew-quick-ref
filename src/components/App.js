973eb5469b18c31e40967f01b5eb7685b74aabec
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-17 20:53:37 -0600

    Added routing skeleton to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// All routing components to be lazy loaded should be like this
+// const Home = React.lazy(() => import('./Home'))
+// const Other = React.lazy(() => import('./Other'))
 
 function App() {
   return (
-    <>
-      <header></header>
-      <main>
-        <p>Welcome to the Stardew Valley Quick Reference</p>
-      </main>
-      <footer></footer>
-    </>
+    <Router>
+      {/* TODO: Suspense fallback loading component */}
+      <React.Suspense fallback="Loading...">
+        {/* All routes must go within suspense to support lazy loading */}
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route path="/other" component={Other} /> */}
+      </React.Suspense>
+    </Router>
   )
 }
 
