625e49bec97b4f85d1b59e1b8a3850be66ceb830
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-19 18:22:46 -0600

    Some foundational styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,36 @@
+/** @jsx jsx */
+
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { css, jsx } from '@emotion/core'
 import LoadingIcon from './LoadingIcon'
 import '../App.css'
 
 // All routing components should be lazy loaded like this
 const Home = React.lazy(() => import('./Home'))
+// const Home = React.lazy(() => new Promise(res => setTimeout(res, 2000)).then(() => import('./Home')))
 // const Other = React.lazy(() => import('./Other'))
+
+const mainStyle = css`
+  background-color: #fff;
+  margin: 0 auto;
+  max-width: 750px;
+  padding: 0 0.5em;
+`
 
 function App() {
   return (
+    // Header here
     <Router>
-      {/* <LoadingIcon /> */}
       {/* All routes must go within suspense to support lazy loading */}
       <React.Suspense fallback={<LoadingIcon />}>
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/other" component={Other} /> */}
+        <main css={mainStyle}>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/other" component={Other} /> */}
+        </main>
       </React.Suspense>
     </Router>
+    // Footer here
   )
 }
 
