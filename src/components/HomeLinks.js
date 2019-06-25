7ba70da41d4014b2060c119eb73025f8eb9a1ea9
Ethan Stewart <ethanstewart@familysearch.org>
2019-06-24 23:43:06 -0600

    Made home nav look like the quest bulletin board

--- src/components/HomeLinks.js
+++ src/components/HomeLinks.js
@@ -4,27 +4,67 @@ import { Link } from 'react-router-dom'
 import { css, jsx } from '@emotion/core'
 
 const navStyle = css`
-  ul {
-    list-style: none;
-    padding: 0;
+  --nav-column-count: 1;
+  background-color: #fbebc2;
+  border: 0.5em solid #744514;
+  border-left-width: calc(0.5em - 2px);
+  border-right-width: calc(0.5em - 2px);
+  display: grid;
+  grid-template-columns: repeat(var(--nav-column-count), 1fr);
+  justify-content: space-between;
+  margin: 0.5em auto 2em;
+  padding: 1em 0;
+  position: relative;
+  transform: translateX(-1em);
+  width: calc(100% + 2em);
+  a {
+    color: #211122;
+    padding: 0.75em 0;
+    text-align: center;
+  }
+  &::before, &::after {
+    --size: 0.75em;
+    background-color: #57656b;
+    border: 3px solid #264651;
+    border-radius: 50%;
+    content: '';
+    height: var(--size);
+    position: absolute;
+    top: calc(var(--size) / -2);
+    width: var(--size);
+  }
+  &::before {
+    left: var(--size);
+  }
+  &::after {
+    right: var(--size);
+  }
+  @media screen and (min-width: 400px) {
+    --nav-column-count: 2;
+  }
+  @media screen and (min-width: 650px) {
+    --nav-column-count: 3;
+  }
+  @media screen and (min-width: 750px) {
+    --nav-column-count: 4;
   }
 `
 
 export default function HomeLinks() {
-  return <nav css={navStyle}>
-    <ul>
-      <li><Link to="/villagers">Villagers</Link></li>
-      <li><Link to="/buildings">Buildings</Link></li>
-      <li><Link to="/skills">Skills</Link></li>
-      <li><Link to="/minerals">Minerals</Link></li>
-      <li><Link to="/artifacts">Artifacts</Link></li>
-      <li><Link to="/fish">Fish</Link></li>
-      <li><Link to="/festivals">Festivals</Link></li>
-      <li><Link to="/crops">Crops</Link></li>
-      <li><Link to="/artisan">Artisan</Link></li>
-      <li><Link to="/shops">Shops</Link></li>
-      <li><Link to="/monsters">Monsters</Link></li>
-      <li><Link to="/crafting">Crafting</Link></li>
-    </ul>
-  </nav>
+  return (
+    <nav css={navStyle}>
+      <Link to="/villagers">Villagers</Link>
+      <Link to="/buildings">Buildings</Link>
+      <Link to="/skills">Skills</Link>
+      <Link to="/minerals">Minerals</Link>
+      <Link to="/artifacts">Artifacts</Link>
+      <Link to="/fish">Fish</Link>
+      <Link to="/festivals">Festivals</Link>
+      <Link to="/crops">Crops</Link>
+      <Link to="/artisan">Artisan</Link>
+      <Link to="/shops">Shops</Link>
+      <Link to="/monsters">Monsters</Link>
+      <Link to="/crafting">Crafting</Link>
+    </nav>
+  )
 }
